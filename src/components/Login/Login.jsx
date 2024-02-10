8215c34130020cba3e245be4c594505f7d3d08c6
tkrupesh14 <info.tkrupesh@gmail.com>
2024-02-11 02:59:53 +0530

    loginAuth

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../../assets/images/logo-main.png'
 import { FaFacebook, FaGoogle } from "react-icons/fa";
+import toast, {Toaster} from 'react-hot-toast';
 
 const Login = () => {
+  const [userData, setUserData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleAuth = (e) => {
+    e.preventDefault();
+    if(userData.email === "johndoe@schoolify.edu" && userData.password === "johndoe"){
+      toast('Welcome! ' + userData.email, {
+        icon: '👏',
+        style: {
+          borderRadius: '10px',
+          background: '#333',
+          color: '#fff',
+        },
+      });
+    }
+    else{
+      toast.error('Something Went Wrong!')
+    }
+  }
   return (
     <div>
+      <Toaster position="top-center"
+  reverseOrder={false}/>
       <div className="container-fluid">
         <div className="row">
           <div className="col d-flex justify-center items-center min-h-screen flex-col">
@@ -20,12 +44,12 @@ const Login = () => {
 
               <form action="" className='ml-4 mt-3'>
                 <label htmlFor="" className='form-label text-white'>Email: </label>
-                <input type="email" name="" id="" className='form-control w-full' placeholder='johndoe@schoolify.edu' />
+                <input type="email" name="" id="" className='form-control w-full' placeholder='johndoe@schoolify.edu' value={userData.email} onChange={(e)=> setUserData({...userData, email: e.target.value})}/>
                 <p className='mt-2 text-white text-right'>Don't have account? <a href="/" className='underline'>Sign Up</a></p>
                 <label htmlFor="" className='form-label text-white'>Password: </label>
-                <input type="password" name="" id="" className='form-control w-full' />
+                <input type="password" name="" id="" className='form-control w-full' value={userData.password} onChange={(e)=> setUserData({...userData, password: e.target.value})}/>
                 <p className='mt-2 text-white text-right'> <a href="/" className='underline'>Forgot Password?</a></p>
-                <div className="login-button d-flex justify-center"><button className="p-2 bg-[#D9D9D9] rounded w-[100px] font-bold drop-shadow-lg">Login</button></div>
+                <div className="login-button d-flex justify-center"><button className="p-2 bg-[#D9D9D9] rounded w-[100px] font-bold drop-shadow-lg" onClick={handleAuth}>Login</button></div>
               </form>
             </div>
 
