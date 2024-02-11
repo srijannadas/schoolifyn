4b002811b3d6dabc5f3550c024431f60ff61f17f
srijannadas <srijanna.2002@gmail.com>
2024-02-12 00:20:07 +0530

    header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Welcome to My Website</h1>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Class</a></li>
+          <li><a href="#">MockTest</a></li>
+          <li><a href="#">Notes</a></li>
+          <li><a href="#">PYQ</a></li>
+          <li><a href="#">Test Potal</a></li>
+          <li><a href="#">Profile</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
