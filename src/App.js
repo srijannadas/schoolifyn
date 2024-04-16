d908b3276cd83493214f31f07d70c18a16f54854
srijannadas <srijann.2002@gmail.com>
2024-04-16 13:13:36 +0530

    Notes

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Login2FA from './components/Login/Login2FA';
 import ProfilePage from './components/Profile/Profile';
 import AdminHome from './components/AdminPanel/Home';
 import CompleteProfile from './components/Profile/CompleteProfile'
+import Notes from './components/Notes/Notes';
 
 function App() {
   // const isLogin = useAuth();
@@ -28,7 +29,7 @@ function App() {
           <Route path='/Login2FA/' element={<Login2FA/>}/>
           <Route path='/profile/' element={<ProfilePage/>}/>
           <Route path='/profile/complete' element={<CompleteProfile/>}/>
-
+          <Route path='/Notes' element={<Notes/>}/>
           <Route path="/admin" element={<AdminHome/>}/>
         </Routes>
       </BrowserRouter>
